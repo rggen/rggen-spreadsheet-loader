582d057393007ea9f7b92a765534037b1853567b
taichi <taichi730@gmail.com>
2019-02-25 19:46:46 +0900

    removed roo_table_foramtter

--- lib/rggen/spreadsheet_loader/roo_loader.rb
+++ lib/rggen/spreadsheet_loader/roo_loader.rb
@@ -7,14 +7,16 @@ module RgGen
 
       def read_spreadsheet(file, book)
         Roo::Spreadsheet.open(file).each_with_pagename do |pagename, page|
-          page.first_row && parse_page(pagename, page, book)
+          page.first_row && book.add_sheet(pagename, collect_cells(page))
         end
       end
 
       private
 
-      def parse_page(pagename, page, book)
-        book.add_sheet(pagename, page.to_table(from_row: 1, from_column: 1))
+      def collect_cells(page)
+        (1..page.last_row).map do |row|
+          (1..page.last_column).map { |column| page.cell(row, column) }
+        end
       end
     end
   end
