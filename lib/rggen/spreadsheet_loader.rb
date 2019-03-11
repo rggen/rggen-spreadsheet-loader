fd7415aa20f25ea4976e592f199dcfcbd13fa219
taichi <taichi730@gmail.com>
2019-03-11 11:36:03 +0900

    changed definition location of SpreadSheetLoader.setup method

--- lib/rggen/spreadsheet_loader.rb
+++ lib/rggen/spreadsheet_loader.rb
@@ -10,4 +10,15 @@ require_relative 'spreadsheet_loader/spreadsheet/sheet'
 require_relative 'spreadsheet_loader/spreadsheet/book'
 require_relative 'spreadsheet_loader/spreadsheet_loader'
 require_relative 'spreadsheet_loader/roo_loader'
-require_relative 'spreadsheet_loader/setup'
+
+module RgGen
+  module SpreadsheetLoader
+    def self.setup(builder)
+      builder.register_loader(:register_map, RooLoader)
+    end
+  end
+
+  setup do |builder|
+    SpreadsheetLoader.setup(builder)
+  end
+end
