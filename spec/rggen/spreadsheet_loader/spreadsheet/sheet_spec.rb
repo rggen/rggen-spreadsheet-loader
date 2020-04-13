4a1bf3830fab90d30724e2157b953f6ad8eab2fd
taichi <taichi730@gmail.com>
2020-04-13 23:21:14 +0900

    changed spec format

--- spec/rggen/spreadsheet_loader/spreadsheet/sheet_spec.rb
+++ spec/rggen/spreadsheet_loader/spreadsheet/sheet_spec.rb
@@ -1,102 +1,98 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+RSpec.describe RgGen::SpreadsheetLoader::Spreadsheet::Sheet do
+  let(:sheet) { described_class.new(book, sheet_name) }
 
-module RgGen::SpreadsheetLoader::Spreadsheet
-  describe Sheet do
-    let(:sheet) { Sheet.new(book, sheet_name) }
+  let(:file) { 'foo.xlsx' }
 
-    let(:file) { 'foo.xlsx' }
+  let(:book) { double('book', file: file) }
 
-    let(:book) { double('book', file: file) }
+  let(:sheet_name) { 'a sheet' }
 
-    let(:sheet_name) { 'a sheet' }
+  let(:table) do
+    Array.new(3) do |row|
+      Array.new(3) { |column| cell_value(row, column) }
+    end
+  end
 
-    let(:table) do
-      Array.new(3) do |row|
-        Array.new(3) { |column| cell_value(row, column) }
+  let(:flat_table) do
+    table.flat_map.with_index do |row, row_index|
+      row.map.with_index do |value, column_index|
+        [row_index, column_index, value]
       end
     end
+  end
 
-    let(:flat_table) do
-      table.flat_map.with_index do |row, row_index|
-        row.map.with_index do |value, column_index|
-          [row_index, column_index, value]
+  def cell_value(row, column)
+    [(3 * row + column), 'foo', :foo, nil].sample
+  end
+
+  def cell_position(row, column)
+    RgGen::SpreadsheetLoader::Spreadsheet::Position.new(file, sheet_name, row, column)
+  end
+
+  describe '#from_table' do
+    it '与えられたテーブルで初期化する' do
+      sheet.from_table(table)
+      flat_table.each do |row, column, value|
+        if value.nil?
+          expect(sheet[row, column]).to be_empty_cell(cell_position(row, column))
+        else
+          expect(sheet[row, column]).to match_value(table[row][column], cell_position(row, column))
         end
       end
     end
+  end
 
-    def cell_value(row, column)
-      [(3 * row + column), 'foo', :foo, nil].sample
-    end
-
-    def cell_position(row, column)
-      Position.new(file, sheet_name, row, column)
-    end
-
-    describe '#from_table' do
-      it '与えられたテーブルで初期化する' do
-        sheet.from_table(table)
-        flat_table.each do |row, column, value|
+  describe '#rows' do
+    def match_cells(values, row_index)
+      matchers = []
+      values.each_with_index do |value, column_index|
+        matchers <<
           if value.nil?
-            expect(sheet[row, column]).to be_empty_cell(cell_position(row, column))
+            be_empty_cell(cell_position(row_index, column_index))
           else
-            expect(sheet[row, column]).to match_value(table[row][column], cell_position(row, column))
+            match_value(value, cell_position(row_index, column_index))
           end
+      end
+      match(matchers)
+    end
+
+    before { sheet.from_table(table) }
+
+    context '無引数の場合' do
+      it '表中の行を返す' do
+        rows = sheet.rows
+        rows.each_with_index do |row, row_index|
+          expect(row.cells).to match_cells(table[row_index], row_index)
         end
       end
     end
 
-    describe '#rows' do
-      def match_cells(values, row_index)
-        matchers = []
-        values.each_with_index do |value, column_index|
-          matchers <<
-            if value.nil?
-              be_empty_cell(cell_position(row_index, column_index))
-            else
-              match_value(value, cell_position(row_index, column_index))
-            end
-        end
-        match(matchers)
-      end
+    context '開始行が指定された場合' do
+      let(:from) { [0, 1, 2].sample }
 
-      before { sheet.from_table(table) }
+      let(:length) { 3 - from }
 
-      context '無引数の場合' do
-        it '表中の行を返す' do
-          rows = sheet.rows
-          rows.each_with_index do |row, row_index|
-            expect(row.cells).to match_cells(table[row_index], row_index)
-          end
+      it '指定された開始行からの行を返す' do
+        rows = sheet.rows(from)
+        expect(rows.size).to eq length
+        rows.each.with_index(from) do |row, row_index|
+          expect(row.cells).to match_cells(table[row_index], row_index)
         end
       end
+    end
 
-      context '開始行が指定された場合' do
-        let(:from) { [0, 1, 2].sample }
+    context '開始行と長さが指定された場合' do
+      let(:from) { [0, 1, 2].sample }
 
-        let(:length) { 3 - from }
+      let(:length) { (1..(3 - from)).to_a.sample }
 
-        it '指定された開始行からの行を返す' do
-          rows = sheet.rows(from)
-          expect(rows.size).to eq length
-          rows.each.with_index(from) do |row, row_index|
-            expect(row.cells).to match_cells(table[row_index], row_index)
-          end
-        end
-      end
-
-      context '開始行と長さが指定された場合' do
-        let(:from) { [0, 1, 2].sample }
-
-        let(:length) { (1..(3 - from)).to_a.sample }
-
-        it '指定された行から、長さ分行を返す' do
-          rows = sheet.rows(from, length)
-          expect(rows.size).to eq length
-          rows.each.with_index(from) do |row, row_index|
-            expect(row.cells).to match_cells(table[row_index], row_index)
-          end
+      it '指定された行から、長さ分行を返す' do
+        rows = sheet.rows(from, length)
+        expect(rows.size).to eq length
+        rows.each.with_index(from) do |row, row_index|
+          expect(row.cells).to match_cells(table[row_index], row_index)
         end
       end
     end
--- spec/rggen/spreadsheet_loader_spec.rb
+++ spec/rggen/spreadsheet_loader_spec.rb
@@ -1,19 +1,15 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+RSpec.describe RgGen::SpreadsheetLoader do
+  describe '既定セットアップ' do
+    let(:builder) { RgGen.builder }
 
-module RgGen
-  describe SpreadsheetLoader do
-    describe '既定セットアップ' do
-      let(:builder) { RgGen.builder }
-
-      it 'builderにCSVLoader/RooLoader/XLSLoaderを登録する' do
-        expect(builder).to receive(:register_loader).with(:register_map, equal(RgGen::SpreadsheetLoader::CSVLoader))
-        expect(builder).to receive(:register_loader).with(:register_map, equal(RgGen::SpreadsheetLoader::RooLoader))
-        expect(builder).to receive(:register_loader).with(:register_map, equal(RgGen::SpreadsheetLoader::XLSLoader))
-        require 'rggen/spreadsheet_loader/setup'
-        builder.activate_plugins
-      end
+    it 'builderにCSVLoader/RooLoader/XLSLoaderを登録する' do
+      expect(builder).to receive(:register_loader).with(:register_map, equal(RgGen::SpreadsheetLoader::CSVLoader))
+      expect(builder).to receive(:register_loader).with(:register_map, equal(RgGen::SpreadsheetLoader::RooLoader))
+      expect(builder).to receive(:register_loader).with(:register_map, equal(RgGen::SpreadsheetLoader::XLSLoader))
+      require 'rggen/spreadsheet_loader/setup'
+      builder.activate_plugins
     end
   end
 end
