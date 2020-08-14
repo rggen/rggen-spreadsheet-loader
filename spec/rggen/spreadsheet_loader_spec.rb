79f0c5a59e5a9c7ed18f2101299af5e46175a02a
Taichi Ishitani <taichi730@gmail.com>
2020-08-15 01:04:14 +0900

    adapted for rggen/rggen-core#63

--- spec/rggen/spreadsheet_loader_spec.rb
+++ spec/rggen/spreadsheet_loader_spec.rb
@@ -19,8 +19,7 @@ RSpec.describe RgGen::SpreadsheetLoader do
       expect(builder).to receive(:register_loader).with(:register_map, :spreadsheet, equal(RgGen::SpreadsheetLoader::XLSLoader)).and_call_original
       expect(builder).to receive(:ignore_value).with(:register_map, :spreadsheet, :register_block, :comment).and_call_original
       expect(builder).to receive(:ignore_value).with(:register_map, :spreadsheet, :register, :comment).and_call_original
-      require 'rggen/spreadsheet_loader/setup'
-      builder.activate_plugins
+      builder.load_plugins(['rggen/spreadsheet_loader/setup'], true)
     end
   end
 end
